603cfd613c82b18a264c896b5023e1307d7527ba
skylar <2031499619@qq.com>
2023-07-15 19:32:55 +0800

    feat: add likou 885

--- src/885.spiral-matrix-iii.js
+++ src/885.spiral-matrix-iii.js
@@ -5,30 +5,37 @@
  * @param {number} cStart
  * @return {number[][]}
  */
-var spiralMatrixIII = function(rows, cols, rStart, cStart) {
-  let ans = [[rStart,cStart]]
-  let track = new Array(rows).fill(0).map(() => new Array(cols).fill(0))
-  track[rStart][cStart] = 1
-  let count = 1;
-
-  let dir = [[0, 1], [1, 0], [-0, -1], [-1, 0]]
-  let startD = 0 // 右-下-左-上
-  while(count < 7) {
-    let [x, y] = [rStart + dir[startD][0], cStart + dir[startD][1]]
-    console.log(x, y, count, startD)
-    if (track[x][y] === 0) {
-      track[x][y] = 1;
-      count++
-      ans.push([x, y])
-      startD = (++startD ) % 4
-      rStart = x
-      cStart = y
-    } else {
-      startD = (--startD ) % 4
-      // break
+var spiralMatrixIII = function (rows, cols, rStart, cStart) {
+  let ans = [
+    [rStart, cStart]
+  ]
+  let dr = [0, 1, 0, -1]
+  let dc = [1, 0, -1, 0]
+  let direc = 0;
+  let count = 0; // 当前方向前进步数
+  let maxCount = 1 // 当前该方向最多前进步数,每两个方向加1
+  let changeCount = 0; // 总的改变方向的次数,要依据该数据计算maxCount,每两次maxCount都要加1
+  let num = 1; // 计算已经走过的数据量
+  while (num < rows * cols) {
+    rStart += dr[direc % 4];
+    cStart += dc[direc % 4];
+    if (rStart >= 0 && rStart < rows && cStart >= 0 && cStart < cols) {
+      ans.push([rStart, cStart]);
+      num++;
+    }
+    console.log(rStart, cStart)
+    count++;
+    if (count == maxCount) {
+      count = 0;
+      direc++;
+      changeCount++;
+    }
+    if (changeCount == 2) {
+      changeCount = 0;
+      maxCount++;
     }
   }
-  console.log(track)
+  return ans;
 };
 
-console.log(spiralMatrixIII(5,6,1,4))
+console.log(spiralMatrixIII(5, 6, 1, 4))
