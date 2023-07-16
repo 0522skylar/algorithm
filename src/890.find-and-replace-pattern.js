b59dffdabc9e5430468da40701d1fc2dee815653
skylar <2031499619@qq.com>
2023-07-16 15:55:29 +0800

    feat: add oether method

--- src/890.find-and-replace-pattern.js
+++ src/890.find-and-replace-pattern.js
@@ -3,6 +3,19 @@
  * @param {string} pattern
  * @return {string[]}
  */
+
+const match1 = (word, pattern) => {
+  const map = new Map();
+  for (let i = 0; i < word.length; ++i) {
+      const x = word[i], y = pattern[i];
+      if (!map.has(x)) {
+          map.set(x, y);
+      } else if (map.get(x) !== y) { // word 中的同一字母必须映射到 pattern 中的同一字母上
+          return false;
+      }
+  }
+  return true;
+}
 var findAndReplacePattern = function(words, pattern) {
 
   let ans = []
