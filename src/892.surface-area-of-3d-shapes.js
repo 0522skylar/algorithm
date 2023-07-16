87118fe786bf75fa6b0d0f00d7fdcaed5986aa36
skylar <2031499619@qq.com>
2023-07-16 16:10:45 +0800

    feat: add likou 892

--- src/892.surface-area-of-3d-shapes.js
+++ src/892.surface-area-of-3d-shapes.js
@@ -6,9 +6,9 @@ var surfaceArea = function(grid) {
   let ans = 0;
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[0].length; j++) {
-      ans += grid[i][j] * 6
+      ans += grid[i][j] * 6  // 一个的表面积为6
       if (grid[i][j] > 1) {
-        ans -= (grid[i][j] - 1) * 2
+        ans -= (grid[i][j] - 1) * 2  // 一个的表面积为6
       }
       if (i >= 1) {
         ans -= Math.min(grid[i][j], grid[i-1][j]) * 2
@@ -16,6 +16,8 @@ var surfaceArea = function(grid) {
       if (j >= 1) {
         ans -= Math.min(grid[i][j], grid[i][j-1]) * 2
       }
+      // grid[i][j]会与grid[i - 1][j], grid[i][j - 1]有接触
+      // 接触面个数为二者的最小,每一个接触面面积同样也是2,减去
     }
   }
   return ans;
