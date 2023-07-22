0618f59f50365adce02daa3f4aa5906d141f79b3
skylar <2031499619@qq.com>
2023-07-22 16:45:25 +0800

    feat: add likou 894

---
+++ src/894.all-possible-full-binary-trees.js
@@ -0,0 +1,79 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number} n
+ * @return {TreeNode[]}
+ */
+// 新建树节点类
+function TreeNode (val, left, right) {
+  this.val = (val === undefined) ? 0 : val;
+  this.left = (left === undefined) ? null : left;
+  this.right = (right === undefined) ? null : right;
+}
+function buildTree(val_list) {
+// 数组为空
+if(!val_list || val_list.length === 0) {
+  return ;
+  }
+// 根节点
+var root = new TreeNode(val_list.shift());
+
+var nodeQueue = [root];
+// 对root节点进行操作，更新node
+while(val_list.length > 0){
+  var node  = nodeQueue.shift();
+  // n = node.level + 1;	// 获取父节点的层级，子节点在该层级上+1
+  // 构建：左孩子节点
+  if(val_list.length === 0) {
+    break;
+      }
+  var leftVal = val_list.shift();
+  if(leftVal != null) {
+    node.left = new TreeNode(leftVal);
+    nodeQueue.push(node.left);
+  }
+  // 构建：右孩子节点
+  if(val_list.length === 0) {
+    break;
+      }
+  var rightVal = val_list.shift();
+  if(rightVal != null) {
+    node.right = new TreeNode(rightVal);
+    nodeQueue.push(node.right);
+  }
+}
+return root;
+}
+var allPossibleFBT = function(n) {
+  let ans = []
+  if (!n % 2) {
+    return ans;
+  } 
+  if (n === 1) {
+    let head = new TreeNode(0)
+    ans.push(head)
+    return ans
+  }
+  for (let i = 1; i < n; i+=2) {
+    let left = allPossibleFBT(i)
+    let right = allPossibleFBT(n - 1- i) 
+
+    for(let item of left) {
+      for(let key of right) {
+        let head = new TreeNode(0)
+        head.left = item
+        head.right = key
+        ans.push(head)
+      }
+    }
+  }
+  return ans;
+};
+
+console.log(allPossibleFBT(7))
