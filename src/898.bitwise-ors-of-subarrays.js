d13f9b415a77423744d70df682dc4749f2bbf299
skylar <2031499619@qq.com>
2023-07-30 18:30:54 +0800

    feat: add likou 898

---
+++ src/898.bitwise-ors-of-subarrays.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var subarrayBitwiseORs = function(arr) {
+  let ans = new Set()
+  let cur = new Set()
+  for(let item of arr) {
+    let next = new Set()
+    for (let orSum of cur) {
+      // console.log(orSum)
+      next.add(orSum | item)
+    }
+
+    cur = next
+    cur.add(item)
+    // console.log(...cur)
+    for (let key of cur) {
+      ans.add(key)
+    }
+    
+  }
+  // console.log(ans)
+  return ans.size
+};
+
+console.log(subarrayBitwiseORs([1,1,2])) // 3
+console.log(subarrayBitwiseORs([1,2,4])) // 6
