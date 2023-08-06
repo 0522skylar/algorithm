85a364d2b4cb7eec16f7b4989c0722135f0a9f8d
skylar <2031499619@qq.com>
2023-08-06 22:27:35 +0800

    feat: add likou 910

---
+++ src/910.smallest-range-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var smallestRangeII = function(nums, k) {
+  nums.sort((a, b) => a - b)
+  console.log(nums)
+  let ans = nums[nums.length - 1] - nums[0]
+  for (let i = 1; i < nums.length; i++) {
+    let min = Math.min(nums[0] + k, nums[i] - k)
+    let max = Math.max(nums[nums.length - 1] - k, nums[i - 1] +k)
+    ans = Math.min(max - min, ans)
+  }
+  return ans;
+};
+console.log(smallestRangeII([1], 0))
+console.log(smallestRangeII([0, 10], 2))
+console.log(smallestRangeII([1,3,6], 3))
