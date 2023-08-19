07c725a25114944e0eb9a10ab45a473c5df0850c
skylar <2031499619@qq.com>
2023-08-19 16:07:16 +0800

    feat: add tijie

--- src/918.maximum-sum-circular-subarray.js
+++ src/918.maximum-sum-circular-subarray.js
@@ -25,3 +25,32 @@ console.log(maxSubarraySumCircular([5,-3,5]))
 console.log(maxSubarraySumCircular([3,-2,2,-3]))
 console.log(maxSubarraySumCircular([-3,-2,-3]))
 
+
+var maxSubarraySumCircular = function(nums) {
+  let n = nums.length;
+  const leftMax = new Array(n).fill(0);
+  // 对坐标为 0 处的元素单独处理，避免考虑子数组为空的情况
+  leftMax[0] = nums[0];
+  let leftSum = nums[0];
+  let pre = nums[0];
+  let res = nums[0];
+  for (let i = 1; i < n; i++) {
+      pre = Math.max(pre + nums[i], nums[i]);
+      res = Math.max(res, pre);
+      leftSum += nums[i];
+      leftMax[i] = Math.max(leftMax[i - 1], leftSum);
+  }
+
+  // 从右到左枚举后缀，固定后缀，选择最大前缀
+  let rightSum = 0;
+  for (let i = n - 1; i > 0; i--) {
+      rightSum += nums[i];
+      res = Math.max(res, rightSum + leftMax[i - 1]);
+  }
+  return res;
+};
+
+// 作者：力扣官方题解
+// 链接：https://leetcode.cn/problems/maximum-sum-circular-subarray/solutions/2350660/huan-xing-zi-shu-zu-de-zui-da-he-by-leet-elou/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
