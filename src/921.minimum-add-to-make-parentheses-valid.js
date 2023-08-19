cb98a64b5fd04c8abf167fe141843c0626fcf2ae
skylar <2031499619@qq.com>
2023-08-19 16:06:05 +0800

    feat: add tijie

--- src/921.minimum-add-to-make-parentheses-valid.js
+++ src/921.minimum-add-to-make-parentheses-valid.js
@@ -23,4 +23,31 @@ var minAddToMakeValid = function(s) {
 // console.log(minAddToMakeValid("())"))
 // console.log(minAddToMakeValid("((("))
 
+
+var sortArrayByParityII = function(nums) {
+  const n  = nums.length;
+  const ans = new Array(n);
+  let i = 0;
+  for (const x of nums) {
+      if (!(x & 1)) {
+          ans[i] = x;
+          i += 2;
+      } 
+  }
+
+  i = 1;
+  for (const x of nums) {
+      if (x & 1) {
+          ans[i] = x;
+          i += 2;
+      }
+  }
+
+  return ans;
+};
+
+// 作者：力扣官方题解
+// 链接：https://leetcode.cn/problems/sort-array-by-parity-ii/solutions/481450/an-qi-ou-pai-xu-shu-zu-ii-by-leetcode-solution/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 console.log(minAddToMakeValid("()))((")) // 4
