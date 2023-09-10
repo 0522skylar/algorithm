2086bdebf452df0266a3bd07230df0943fe5ea09
skylar <2031499619@qq.com>
2023-09-10 15:55:04 +0800

    feat: add likou 942 ans

--- src/942.di-string-match.js
+++ src/942.di-string-match.js
@@ -20,3 +20,18 @@ var diStringMatch = function(s) {
 console.log(diStringMatch("IDID"))
 console.log(diStringMatch("III"))
 console.log(diStringMatch("DDI"))
+
+var diStringMatch = function(s) {
+  let n = s.length, lo = 0, hi = n;
+  const perm = new Array(n + 1).fill(0);
+  for (let i = 0; i < n; ++i) {
+      perm[i] = s[i] === 'I' ? lo++ : hi--;
+  }
+  perm[n] = lo; // 最后剩下一个数，此时 lo == hi
+  return perm;
+};
+
+// 作者：力扣官方题解
+// 链接：https://leetcode.cn/problems/di-string-match/solutions/1473721/zeng-jian-zi-fu-chuan-pi-pei-by-leetcode-jzm2/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
