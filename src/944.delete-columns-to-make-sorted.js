b8d2628757b56ed38bf6766ff443dba8262eb2df
skylar <2031499619@qq.com>
2023-09-16 17:52:12 +0800

    feat: add likou 944 ans

--- src/944.delete-columns-to-make-sorted.js
+++ src/944.delete-columns-to-make-sorted.js
@@ -25,3 +25,23 @@ var minDeletionSize = function(strs) {
 console.log(minDeletionSize(["cba","daf","ghi"]))
 console.log(minDeletionSize(['a', 'b']))
 console.log(minDeletionSize(["zyx","wvu","tsr"]))
+
+var minDeletionSize = function(strs) {
+  const row = strs.length;
+  const col = strs[0].length;
+  let ans = 0;
+  for (let j = 0; j < col; ++j) {
+      for (let i = 1; i < row; ++i) {
+          if (strs[i - 1][j] > strs[i][j]) {
+              ans++;
+              break;
+          }
+      }
+  }
+  return ans;
+};
+
+// 作者：力扣官方题解
+// 链接：https://leetcode.cn/problems/delete-columns-to-make-sorted/solutions/1483008/shan-lie-zao-xu-by-leetcode-solution-bqyy/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
