d5b03a518d11d1aba478b1ad295f8b65e3d8195f
skylar <2031499619@qq.com>
2023-09-16 16:59:49 +0800

    feat: add likou 945 ans

--- src/945.minimum-increment-to-make-array-unique.js
+++ src/945.minimum-increment-to-make-array-unique.js
@@ -47,6 +47,25 @@ var minIncrementForUnique = function(nums) {
   }
   return ans;
 }
+
+
+var minIncrementForUnique = function(A) {
+  A.sort((a,b) => a-b);
+  var count=0;
+  for(var i=1;i<A.length;i++){
+      if(A[i]<=A[i-1]){
+          var pre = A[i];
+          A[i]=A[i-1]+1;
+          count += A[i]-pre; 
+      }
+  }
+  return count;
+};
+
+// 作者：阿巴阿巴
+// 链接：https://leetcode.cn/problems/minimum-increment-to-make-array-unique/solutions/1673860/javascriptban-jie-ti-si-lu-by-ityou-o-r0cm/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 console.log(minIncrementForUnique([1,2,2]))
 console.log(minIncrementForUnique([3,2,1,2,1,7]))
 console.log(minIncrementForUnique([2,2,2,1]))
