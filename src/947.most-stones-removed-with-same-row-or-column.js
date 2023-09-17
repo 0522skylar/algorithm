c728f9c0fe542bf0eca8622fca90cf9064356897
skylar <2031499619@qq.com>
2023-09-17 16:26:11 +0800

    feat: add likou 947 ans2

--- src/947.most-stones-removed-with-same-row-or-column.js
+++ src/947.most-stones-removed-with-same-row-or-column.js
@@ -75,5 +75,64 @@ const dfs = (x, vis, edge) => {
   
 }
 
+
+var removeStones = function(stones) {
+  const dsu = new DisjointSetUnion();
+  for (const [x, y] of stones) {
+      dsu.unionSet(x, y + 10001);
+  }
+  return stones.length - dsu.numberOfConnectedComponent();
+};
+
+class DisjointSetUnion {
+  constructor() {
+      this.f = new Map();
+      this.rank = new Map();
+  }
+
+  find (x) {
+      if (!this.f.has(x)) {
+          this.f.set(x, x);
+          this.rank.set(x, 1);
+          return x;
+      }
+      if (this.f.get(x) === x) {
+          return x;
+      }
+      this.f.set(x, this.find(this.f.get(x)));
+      return this.f.get(x);
+  }
+
+  unionSet (x, y) {
+      let fx = this.find(x), fy = this.find(y);
+      if (fx  && fy) {
+
+      }
+      if (fx === fy) {
+          return;
+      }
+      if (this.rank.get(fx) < this.rank.get(fy)) {
+          [fx, fy] = [fy, fx];
+      }
+      this.rank.set(fx, this.rank.get(fy) + this.rank.get(fx));
+      this.f.set(fy, fx);
+  }
+
+  numberOfConnectedComponent () {
+      let sum = 0;
+      for (const [x, fa] of this.f.entries()) {
+          if (x === fa) {
+              sum++;
+          }
+      }
+      return sum;
+  }
+}
+
+// 作者：力扣官方题解
+// 链接：https://leetcode.cn/problems/most-stones-removed-with-same-row-or-column/solutions/560363/yi-chu-zui-duo-de-tong-xing-huo-tong-lie-m50r/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
 console.log(removeStones([[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]]))
 console.log(removeStones([[0,0],[0,2],[1,1],[2,0],[2,2]]))
