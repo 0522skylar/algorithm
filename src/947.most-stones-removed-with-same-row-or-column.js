d78d51a5cd245cacd13074f77b3a3b476d2a3fd5
skylar <2031499619@qq.com>
2023-09-17 16:25:56 +0800

    feat: add likou 947 ans1

--- src/947.most-stones-removed-with-same-row-or-column.js
+++ src/947.most-stones-removed-with-same-row-or-column.js
@@ -34,5 +34,46 @@ var removeStones = function(stones) {
   return n - num
 };
 
+// 优化建图
+var removeStones1 = function(stones) {
+  const n = stones.length;
+  const edge = {};
+  const rec = {};
+  for (const [i, [x, y]] of stones.entries()) {
+      rec[x] ? rec[x].push(i) : rec[x] = [i];
+      rec[y + 10001] ? rec[y + 10001].push(i) : rec[y + 10001] = [i];
+  }
+
+  for (const vec of Object.values(rec)) {
+      const k = vec.length;
+      for (let i = 1; i < k; i++) {
+          edge[vec[i - 1]] ? edge[vec[i - 1]].push(vec[i]) : edge[vec[i - 1]] = [vec[i]];
+          edge[vec[i]] ? edge[vec[i]].push(vec[i - 1]) : edge[vec[i]] = [vec[i - 1]];
+      }
+  }
+
+  const vis = new Set();
+  let num = 0;
+  for (let i = 0; i < n; i++) {
+      if (!vis.has(i)) {
+          num++;
+          dfs(i, vis, edge);
+      }
+  }
+  return n - num;
+};
+
+const dfs = (x, vis, edge) => {
+  vis.add(x);
+  if (edge[x]){
+      for (const y of edge[x]) {
+          if (!vis.has(y)) {
+              dfs(y, vis, edge);
+          }
+      }
+  }
+  
+}
+
 console.log(removeStones([[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]]))
 console.log(removeStones([[0,0],[0,2],[1,1],[2,0],[2,2]]))
