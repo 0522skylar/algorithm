18e092c969118e6a989863316321982745037c02
skylar <2031499619@qq.com>
2023-09-23 15:39:17 +0800

    feat: add likou 953 ans

--- src/953.verifying-an-alien-dictionary.js
+++ src/953.verifying-an-alien-dictionary.js
@@ -25,3 +25,36 @@ var isAlienSorted = function(words, order) {
 
 console.log(isAlienSorted(["word","world","row"], "worldabcefghijkmnpqstuvxyz")) // false
 console.log(isAlienSorted(["hello","leetcode"], "hlabcdefgijkmnopqrstuvwxyz")) // true
+
+
+var isAlienSorted = function(words, order) {
+  const index = new Array(26).fill(0);
+  for (let i = 0; i < order.length; ++i) {
+      index[order[i].charCodeAt() - 'a'.charCodeAt()] = i;
+  }
+  for (let i = 1; i < words.length; i++) {
+      let valid = false;
+      for (let j = 0; j < words[i - 1].length && j < words[i].length; j++) {
+          let prev = index[words[i - 1][j].charCodeAt() - 'a'.charCodeAt()];
+          let curr = index[words[i][j].charCodeAt() - 'a'.charCodeAt()];
+          if (prev < curr) {
+              valid = true;
+              break;
+          } else if (prev > curr) {
+              return false;
+          }
+      }
+      if (!valid) {
+          /* 比较两个字符串的长度 */
+          if (words[i - 1].length > words[i].length) {
+              return false;
+          }
+      }
+  }
+  return true;
+};
+
+// 作者：力扣官方题解
+// 链接：https://leetcode.cn/problems/verifying-an-alien-dictionary/solutions/1496288/yan-zheng-wai-xing-yu-ci-dian-by-leetcod-jew7/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
