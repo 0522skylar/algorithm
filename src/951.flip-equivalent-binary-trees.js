99c52522ff1be08ee3782dc08263342c60eeccf4
skylar <2031499619@qq.com>
2023-09-23 15:17:29 +0800

    feat: add likou 951 ans

--- src/951.flip-equivalent-binary-trees.js
+++ src/951.flip-equivalent-binary-trees.js
@@ -23,3 +23,26 @@ var flipEquiv = function(root1, root2) {
   }
   return false
 };
+
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+var flipEquiv = function (root1, root2) {
+  /** 定义：输入两棵二叉树，判断这两棵二叉树是否是翻转等价的 */
+  // 判断 root1 和 root2 两个节点是否能够匹配
+  if (root1 == null || root2 == null) return root1==root2;
+  if (root1.val != root2.val) return false;
+  // 判断子树是否能够匹配，不翻转、翻转两种情况满足一种即可算是匹配
+  return (
+    (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)) ||
+    (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left))
+  );
+};
+
+
+// 作者：angela
+// 链接：https://leetcode.cn/problems/flip-equivalent-binary-trees/solutions/1189867/bi-xu-miao-dong-4ju-dai-ma-jie-jue-by-an-s662/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
