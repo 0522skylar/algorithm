90eeba46747e8cd986fed7e147ec8beb97ade54a
skylar <2031499619@qq.com>
2023-10-21 18:16:36 +0800

    feat: add likou 965

---
+++ src/965.univalued-binary-tree.js
@@ -0,0 +1,70 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isUnivalTree = function(root) {
+  let value = root.val;
+  const dfs = (root, val) => {
+    if (root == null) {
+      return true
+    }
+    if (root.val !== val) {
+      return false
+    }
+    
+    return dfs(root.right, val) && dfs(root.left, val)
+  }
+
+  return dfs(root, value)
+};
+
+// 新建树节点类
+function TreeNode (val, left, right) {
+  this.val = (val === undefined) ? 0 : val;
+  this.left = (left === undefined) ? null : left;
+  this.right = (right === undefined) ? null : right;
+}
+function buildTree(val_list) {
+// 数组为空
+if(!val_list || val_list.length === 0) {
+  return ;
+  }
+// 根节点
+var root = new TreeNode(val_list.shift());
+
+var nodeQueue = [root];
+// 对root节点进行操作，更新node
+while(val_list.length > 0){
+  var node  = nodeQueue.shift();
+  // n = node.level + 1;	// 获取父节点的层级，子节点在该层级上+1
+  // 构建：左孩子节点
+  if(val_list.length === 0) {
+    break;
+      }
+  var leftVal = val_list.shift();
+  if(leftVal != null) {
+    node.left = new TreeNode(leftVal);
+    nodeQueue.push(node.left);
+  }
+  // 构建：右孩子节点
+  if(val_list.length === 0) {
+    break;
+      }
+  var rightVal = val_list.shift();
+  if(rightVal != null) {
+    node.right = new TreeNode(rightVal);
+    nodeQueue.push(node.right);
+  }
+}
+return root;
+}
+
+console.log(isUnivalTree(buildTree([2,2,2,5,2]))) // f
