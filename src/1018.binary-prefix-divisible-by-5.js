7423dbea693851fad2e4395ac2aa437e9fbb1ba5
skylar <2031499619@qq.com>
2023-11-27 08:51:11 +0800

    feat: add likou 1018

--- src/1018.binary-prefix-divisible-by-5.js
+++ src/1018.binary-prefix-divisible-by-5.js
@@ -6,8 +6,8 @@ var prefixesDivBy5 = function(nums) {
   let ans = []
   let sum = 0
   for (let i = 0; i < nums.length; i++) {
-    sum = sum * 2 + nums[i] 
-    sum %= 5
+    sum = sum * 2 + nums[i] // 使用parseInt(num,2)会出错
+    sum %= 5 // 要使sum变小，不然会有精度问题
     if (sum === 0) {
       ans.push(true)
     } else {
