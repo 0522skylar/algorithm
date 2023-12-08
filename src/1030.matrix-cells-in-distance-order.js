367415e5031858f2748d62db5f00401515a5dd97
skylar <2031499619@qq.com>
2023-12-08 08:59:20 +0800

    feat: add likou 1030

---
+++ src/1030.matrix-cells-in-distance-order.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} rows
+ * @param {number} cols
+ * @param {number} rCenter
+ * @param {number} cCenter
+ * @return {number[][]}
+ */
+var allCellsDistOrder = function(rows, cols, rCenter, cCenter) {
+  let ans = []
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      ans.push([i, j])
+    }
+  }
+  ans.sort((a, b) => {
+    return Math.abs(a[0] - rCenter) + Math.abs(a[1] - cCenter) < Math.abs(b[0] - rCenter) + Math.abs(b[1] - cCenter)
+  })
+  return ans
+};
+
+console.log(allCellsDistOrder(1,2,0,0))
+console.log(allCellsDistOrder(2,3,1,2))
