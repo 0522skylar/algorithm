4f583982fdd0c2f5604f9d1871f977abf38e7afe
skylar <2031499619@qq.com>
2023-12-16 21:25:06 +0800

    feat: add likou 1039 ans

--- src/1039.minimum-score-triangulation-of-polygon.js
+++ src/1039.minimum-score-triangulation-of-polygon.js
@@ -30,3 +30,31 @@ function minScoreTriangulation(values) {
   }
   return dp[0][n-1]
 }
+
+var minScoreTriangulation2 = function(values) {
+  const n = values.length;
+  const memo = new Map();
+  const dp = (i, j) => {
+      if (i + 2 > j) {
+          return 0;
+      }
+      if (i + 2 === j) {
+          return values[i] * values[i + 1] * values[j];
+      }
+      const key = i * n + j;
+      if (!memo.has(key)) {
+          let minScore = Number.MAX_VALUE;
+          for (let k = i + 1; k < j; k++) {
+              minScore = Math.min(minScore, values[i] * values[k] * values[j] + dp(i, k) + dp(k, j));
+          }
+          memo.set(key, minScore);
+      }
+      return memo.get(key);
+  }
+  return dp(0, n - 1);
+};
+
+// 作者：力扣官方题解
+// 链接：https://leetcode.cn/problems/minimum-score-triangulation-of-polygon/solutions/2202467/duo-bian-xing-san-jiao-pou-fen-de-zui-di-weqg/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
