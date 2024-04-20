da8686d045e5d2c8068313ee24c926b631de16e9
skylar <2031499619@qq.com>
2024-04-20 18:41:39 +0800

    feat: pref 80 func

--- src/interview150/80.remove-duplicates-from-sorted-array-ii.js
+++ src/interview150/80.remove-duplicates-from-sorted-array-ii.js
@@ -11,16 +11,35 @@ var removeDuplicates = function(nums) {
       if (count >= 2) {
         continue
       }
+      console.log(123)
       i++;
       nums[i] = nums[j];
     } else {
       count = 0
       i++;
       nums[i] = nums[j];
+      console.log(123)
     }
   }
   // console.log(nums)
   return i + 1
 };
 
-console.log(removeDuplicates([1,1,1,2,2,3]))
+var removeDuplicates1 = function(nums) {
+  const n = nums.length;
+  if (n <= 2) {
+      return n;
+  }
+  let slow = 2, fast = 2;
+  while (fast < n) {
+      if (nums[slow - 2] != nums[fast]) {
+          nums[slow] = nums[fast];
+          console.log(1111)
+          ++slow;
+      }
+      ++fast;
+  }
+  return slow;
+};
+
+console.log(removeDuplicates1([1,1,1,2,2,3]))
