586aa6a17c0cc6072c0d5936fed59a7c7a45f489
skylar <2031499619@qq.com>
2024-06-15 14:47:26 +0800

    feat: add likou 219

--- src/interview150/219.contains-duplicate-ii.js
+++ src/interview150/219.contains-duplicate-ii.js
@@ -37,3 +37,19 @@ console.log(containsNearbyDuplicate([1,0,1,1], 1))
 console.log(containsNearbyDuplicate([1,2,3,1], 3))
 console.log(containsNearbyDuplicate([1,2,3,1,2,3], 2))
 console.log(containsNearbyDuplicate([0,1,2,3,2,5], 3)) // true
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate2 = function(nums, k) {
+  for(var i = 0; i < nums.length; i++) {
+      for(var j = i+1; j < nums.length; j++) {
+          if(nums[j] == nums[i] && j-i <= k) {
+              return true;
+          }
+      }
+  }
+  return false;
+};
